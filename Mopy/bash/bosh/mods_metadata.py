AlexMyddleware/wrye-bash

commit fcdabd874959522ff9edc4cecbe3f410aff77635

bosh._mergeability -> plugin_types.py:
Move plugin types handling at top level - we have enough variety now to
need a centralized model. Note I had to move the mod_files import in
local scope - this is #600 territory, but this commit already simplifies
stuff as GameInfo can freely import isPBashMergeable and MasterFlag. The
former will allow making mergeability checks a dict reducing flags
handling in client code. I added comments and rearranged method
definitions - with the help of the IDE's marking of overrides this
makes it easier to see what are the responsibilities of each (sub)class.

Note _pbash_mergeable_no_load is a quick check for MERGE

Mopy/bash/bosh/mods_metadata.py
@@ -30,7 +30,7 @@ from ..bolt import SubProgress, dict_sort, sig_to_str, structs_cache
 from ..brec import ModReader, RecordHeader, RecordType, ShortFidWriteContext, \
     SubrecordBlob, unpack_header
 from ..exception import CancelError
-from ..game import MergeabilityCheck
+from ..plugin_types import MergeabilityCheck
 from ..mod_files import ModHeaderReader
 from ..wbtemp import TempFile
 
@@ -210,11 +210,12 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     # Check for ESL-flagged plugins that aren't ESL-capable and Overlay-flagged
     # plugins that shouldn't be Overlay-flagged. Also check for conflicts
     # between ESL and Overlay flags.
+    pflags = bush.game.scale_flags
     flag_errors = {k: {h_msg: set() for h_msg in v} for k, v in
-                   bush.game.scale_flags.error_msgs.items()}
+                   pflags.error_msgs.items()}
     for m, modinf in modInfos.items():
-        for pflag in bush.game.scale_flags:
-            if pflag in type(pflag).error_msgs and pflag.cached_type(modinf):
+        for pflag in pflags:
+            if pflag in pflags.error_msgs and pflag.cached_type(modinf):
                 pflag.validate_type(modinf, flag_errors[pflag].values())
     # -------------------------------------------------------------------------
     # Check for Deactivate-tagged plugins that are active and
@@ -598,7 +599,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         if raw_eid:
             ret_fmt = f'{raw_eid} {ret_fmt}'
         return ret_fmt
-    format_fid = bush.game.scale_flags.format_fid
+    format_fid = pflags.format_fid
     def log_collision(coll_fid, coll_inj, coll_plugin, coll_versions):
         """Logs a single collision with the specified FormID, injected status,
         origin plugin and collision info."""
@@ -636,7 +637,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         log(_(u'Wrye Bash could not read the follow plugins. They most likely '
               u'have corrupt or otherwise malformed headers.'))
         log_plugin_messages(all_corrupted) ##: Just _log_plugins?
-    for pflag in bush.game.scale_flags:
+    for pflag in pflags:
         if pflag.merge_check is not None:
             minfos_cache, head, msg = pflag.merge_check.cached_types(modInfos)
             if minfos_cache:

Mopy/bash/_games_lo.py
@@ -40,6 +40,7 @@ from functools import partial
 from itertools import chain
 
 from . import bass, bolt, env, exception
+from .plugin_types import MasterFlag
 from .bolt import AFile, FName, Path, deprint, dict_sort
 from .ini_files import get_ini_type_and_encoding
 
@@ -441,7 +442,6 @@ class LoGame:
         Called in get_load_order() to fix a newly fetched LO and in
         set_load_order() to check if a load order passed in is valid. Needs
         rethinking as saving load and active should be an atomic operation."""
-        from .game import MasterFlag
         old_lord = lord[:]
         # game's master might be out of place (if using timestamps for load
         # ordering or a manually edited loadorder.txt) so move it up
@@ -738,7 +738,6 @@ class TimestampGame(LoGame):
     def __calculate_mtime_order(self, mods=None): # excludes mods in corrupted
         mods = ((k, self.mod_infos[k]) for k in
                 (self.mod_infos if mods is None else mods))
-        from .game import MasterFlag
         is_m = MasterFlag.ESM.cached_type
         return [m for m, _inf in sorted(mods, key=lambda x: (
             # split into master block and not master block then sort by ftime

Mopy/bash/basher/__init__.py
@@ -81,7 +81,7 @@ from ..bosh import ModInfo, omods, RefrData
 from ..bosh.mods_metadata import read_dir_tags, read_loot_tags
 from ..exception import BoltError, CancelError, FileError, SkipError, \
     UnknownListener
-from ..game import MasterFlag
+from ..plugin_types import MasterFlag
 from ..gui import CENTER, BusyCursor, Button, CancelButton, CenteredSplash, \
     CheckListBox, Color, CopyOrMovePopup, DateAndTimeDialog, DropDown, \
     EventResult, FileOpen, GlobalMenu, HLayout, Label, LayoutOptions, \
@@ -375,7 +375,7 @@ class _ModsUIList(UIList):
             item_format.text_key = 'mods.text.bashedPatch'
             mouse_text.append(_('Bashed Patch.'))
         for mchk in bush.game.mergeability_checks:
-            txtkey, mtext = mchk.can_convert_cached(minf, checkMark)
+            txtkey, mtext = mchk.display_info(minf, checkMark)
             if txtkey:
                 item_format.text_key = txtkey
                 mouse_text.append(mtext)

Mopy/bash/basher/constants.py
@@ -24,7 +24,7 @@
 """This module contains some constants ripped out of basher.py"""
 from .. import bush
 from ..bolt import GPath
-from ..game import MergeabilityCheck
+from ..plugin_types import MergeabilityCheck
 from ..gui import DEFAULT_POSITION
 
 # some repetitive strings

Mopy/bash/basher/dialogs.py
@@ -32,7 +32,7 @@ from ..bolt import CIstr, FName, GPath_no_norm, text_wrap, top_level_dirs, \
     reverse_dict
 from ..bosh import ModInfo, faces
 from ..fomod_schema import default_moduleconfig
-from ..game import MasterFlag
+from ..plugin_types import MasterFlag
 from ..gui import BOTTOM, CENTER, RIGHT, AMultiListEditor, CancelButton, \
     CheckBox, CheckListBox, DeselectAllButton, DialogWindow, DocumentViewer, \
     DropDown, EventResult, FileOpen, GridLayout, HBoxedLayout, HLayout, \

Mopy/bash/basher/gui_patchers.py
@@ -31,7 +31,7 @@ from .. import bass, bolt, bosh, bush, load_order
 from ..balt import CheckLink, SeparatorLink
 from ..bolt import FName, dict_sort, forward_compat_path_to_fn, \
     forward_compat_path_to_fn_list, text_wrap
-from ..game import MergeabilityCheck
+from ..plugin_types import MergeabilityCheck
 from ..gui import TOP, Button, CheckBox, CheckListBox, DeselectAllButton, \
     EventResult, FileOpenMultiple, HBoxedLayout, Label, LayoutOptions, \
     ListBox, Links, PanelWin, SearchBar, SelectAllButton, Spacer, TextArea, \

Mopy/bash/basher/links_init.py
@@ -48,7 +48,7 @@ from ..balt import BashStatusBar, MenuLink, SeparatorLink, UIList_Delete, \
     UIList_Hide, UIList_OpenItems, UIList_OpenStore, UIList_Rename
 from ..bolt import os_name
 from ..env import init_app_links
-from ..game import MasterFlag
+from ..plugin_types import MasterFlag
 from ..game.patch_game import PatchGame
 from ..gui import GuiImage, get_image
 

Mopy/bash/basher/mod_links.py
@@ -43,7 +43,7 @@ from ..bass import Store
 from ..bolt import FName, SubProgress, dict_sort, sig_to_str
 from ..brec import RecordType
 from ..exception import BoltError, CancelError, PluginsFullError
-from ..game import MergeabilityCheck, PluginFlag, MasterFlag
+from ..plugin_types import MergeabilityCheck, PluginFlag, MasterFlag
 from ..gui import BmpFromStream, BusyCursor, copy_text_to_clipboard, askText, \
     showError
 from ..localize import format_date

commit c38bf338c9ea775fcc04df8c992127dbccb2bfcf

Fix regression in Mods Checker
I think this broke in fcdabd874959522ff9edc4cecbe3f410aff77635, but not
100% sure. The new format_fid method needs ModInfos passed in.

Mopy/bash/bosh/mods_metadata.py
@@ -598,7 +598,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         """Logs a single collision with the specified FormID, injected status,
         origin plugin and collision info."""
         # FormIDs must be in long format at this point
-        proper_fid = format_fid(coll_fid, coll_plugin)
+        proper_fid = format_fid(coll_fid, coll_plugin, modInfos)
         if coll_inj:
             log('* ' + _('%(injected_formid)s injected into '
                          '%(injection_target)s, colliding versions:') % {
@@ -820,7 +820,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
             for orig_plugin, dupe_count in duplicates_counter.items():
                 log('* ' + _('%(full_fid)s in %(orig_plugin)s: '
                              'occurs %(num_duplicates)d times') % {
-                    'full_fid': format_fid(orig_fid, orig_plugin),
+                    'full_fid': format_fid(orig_fid, orig_plugin, modInfos),
                     'orig_plugin': orig_plugin,
                     'num_duplicates': dupe_count,
                 })